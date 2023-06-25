041d9e6f1a2467ebdfe25b7c63d6b8dc9d81a3b4
Dizzymouse0 <npage1221@gmail.com>
2023-06-25 07:46:01 -0500

    adding questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 let startButton = document.getElementById("start-button");
+let questions = document.getElementById("question");
+
+let questions = [
+  {
+    question: "Are Java and Javascript the same thing?",
+    answers: [
+      { text: "Yes", correct: false },
+      { text: "No", correct: true },
+    ]
+  }
+]
